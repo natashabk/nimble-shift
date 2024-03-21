4fca30d461fb193546da35d33626e594716c76db
Natasha Kaczmarczyk <natasha.k@hey.com>
2024-03-21 10:50:16 -0700

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const CreateUser = () => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submitted data:', {firstName, lastName, phone, email});
+    // Here, you can also send formData to a server or API endpoint
+  };
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={( e ) => setFirstName( e.target.value )}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={( e ) => setLastName( e.target.value )}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={( e ) => setEmail( e.target.value )}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="phone">Phone Number:</label>
+        <input
+          type="tel"
+          name="phone"
+          placeholder="123-456-7890"
+          value={phone}
+          onChange={( e ) => {
+            const numericPhone = e.target.value.replace(/\D/g,'')
+            setPhone( numericPhone )
+          }}
+          required
+        />
+      </div>
+      <button type="submit">Create Profile</button>
+    </form>
+  )
 }
 
-export default App;
+export default CreateUser
